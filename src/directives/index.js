3ad3a3a646ade9ec8934c17671288cd115dce498
Adil <adil_dostmohamed@yahoo.co.uk>
2020-04-06 19:42:27 +0100

    added server and frontend directories, installed frontend

--- src/directives/index.js
+++
@@ -1,9 +0,0 @@
-const AuthorizationDirective = require("./authorization");
-const AuthenticationDirective = require("./authentication");
-
-const schemaDirectives = {
-  authorization: AuthorizationDirective,
-  authentication: AuthenticationDirective,
-};
-
-module.exports = schemaDirectives;
