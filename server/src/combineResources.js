c98924d45f33fb501766191184a2ec7962c1d4d6
Adil <adil_dostmohamed@yahoo.co.uk>
2020-06-07 16:33:45 +0100

    added controllers

--- server/src/combineResources.js
+++ server/src/combineResources.js
@@ -1,12 +1,13 @@
 const merge = require("lodash.merge");
+// import custom scalar definitions
+const date = require("./scalars/date");
 // import app resouces to construct gql server
 const user = require("./resources/user");
-
 // construct typedefs from resources
-const typeDefs = [user.typeDefs].join(" ");
+const typeDefs = [date.typeDefs, user.typeDefs].join(" ");
 
 // combine resolvers from all resources
-const resolvers = merge(user.resolvers);
+const resolvers = merge(date.resolvers, user.resolvers);
 
 // combine models from all resources
 const models = {
